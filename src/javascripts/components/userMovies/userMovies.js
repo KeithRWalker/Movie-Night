1f7b079b1b2b4d7157155f228517a9f66d11edec
Keith Walker <Keithrwalker418@gmail.com>
2019-06-04 18:36:19 -0500

    WIP (group project break)

--- src/javascripts/components/userMovies/userMovies.js
+++ src/javascripts/components/userMovies/userMovies.js
@@ -4,7 +4,15 @@ import 'firebase/auth';
 import userMovieData from '../../helpers/data/userMovieData';
 import allMovies from '../movies/movies';
 
+const displayAllMoviesFromUser = () => {
+  const allMoviesLink = document.getElementById('moviesLink');
+  const formCon = document.getElementById('formCon');
+  formCon.classList.add('hide');
+  allMoviesLink.addEventListener('click', allMovies.displayMovies);
+};
+
 const displayUserMovies = () => {
+  displayAllMoviesFromUser();
   const user = firebase.auth().currentUser.uid;
   userMovieData.loadUserMovies(user)
     .then((resp) => { allMovies.movieBuild(resp); })
